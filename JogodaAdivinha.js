cc304ef0181e3f17c14163e3415c03244495f6ab
João Vitor Porto <joaovitorportosales46@gmail.com>
2024-03-12 10:38:33 -0300

    refiz a alteração

--- JogodaAdivinha.js
+++ JogodaAdivinha.js
@@ -1,11 +1,11 @@
-let aleatorio = (Math.floor(Math.random(1) * 201));
+let aleatorio = (Math.floor(Math.random(1) * 101));
 
-let pessoa = (Math.floor(Math.random(1) * 201));
+let pessoa = (Math.floor(Math.random(1) * 101));
 
 let tentativa = 0;
 
 while (aleatorio != pessoa) {
-    pessoa = Math.floor(Math.random(1) * 201);
+    pessoa = Math.floor(Math.random(1) * 101);
     if (pessoa > aleatorio) {
         console.log("Tente novamente, o numero secreto é menor", pessoa);
     } else if (pessoa < aleatorio) {
